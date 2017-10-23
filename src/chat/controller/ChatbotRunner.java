f56cfc785f5eb60197dc65c75ddf361d9b85c0f5
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-23 12:46:01 -0600

    Changed the runner class to the actual main method.

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -1,13 +1,10 @@
 package chat.controller;
 
-import chat.view.PopupDisplay;
-
 public class ChatbotRunner
 {
 	public static void main(String [] args)
 	{
-		PopupDisplay test = new PopupDisplay();
-		test.displayText("Hi");
-		test.collectResponse("How are you?");
+		ChatbotController app = new ChatbotController();
+		app.start();
 	}
 }
