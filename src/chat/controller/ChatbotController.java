a3c1bdf88907f818c1afcf19aa3db0148f091409
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-01 12:48:14 -0600

    Made multiple methods in the controller class to talk to the chatbot.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,9 +1,40 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.PopupDisplay;
+
 public class ChatbotController
 {
+	private Chatbot chatbot;
+	private PopupDisplay display;
+	
+	public ChatbotController()
+	{
+		chatbot = new Chatbot("Oshabot");
+		display = new PopupDisplay();
+	}
 	public void start()
 	{
+		String response = display.collectResponse("What do you want to talk about?");
 		
+		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+		{
+			response = popupChat(response);
+			response = display.collectResponse(response);
+		}
+	}
+	
+//	public String interactWithChatbot(String input)
+//	{
+//		
+//	}
+	
+	private String popupChat(String chat)
+	{
+		String chatbotSays = "";
+		
+		chatbotSays += chatbot.processConversation(chat);
+		
+		return chatbotSays;
 	}
 }
