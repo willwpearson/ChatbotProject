377220cffde9bec3944a2e5b111f983910d897c0
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-13 13:45:49 -0700

    Added the basic framework for the Panel class.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,24 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
+	private ChatbotController appController;
+	private ChatPanel appPane;
 	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with Oshabot");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 }
---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,37 @@
+package chat.view;
+
+import javax.swing.JPanel;
+import chat.controller.ChatbotController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	
+	
+	public ChatPanel(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
