84497bd22a6151685fd48057df5b970f61f34937
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-15 12:39:44 -0700

    Imported the necessary toolkits, and declared data members for each of the needed tools.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -5,14 +5,16 @@ import chat.controller.ChatbotController;
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.SpringLayout;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
 
 public class ChatPanel extends JPanel
 {
 	private ChatbotController appController;
-	
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
 	
 	public ChatPanel(ChatbotController appController)
 	{
