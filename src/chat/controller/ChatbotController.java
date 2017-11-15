512605a9357db0fe2b0cad1e1ffdca2d5ff55154
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-15 12:26:14 -0700

    Referenced ChatFrame in the controller.

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -2,27 +2,30 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatbotController
 {
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appFrame;
 	
 	public ChatbotController()
 	{
 		chatbot = new Chatbot("Oshabot");
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
 	public void start()
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 		
 		// Loops the responses over and over until an invalid response or quit is given.
-		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response = display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response) && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response = display.collectResponse(response);
+//		}
 	}
 	
 	private String popupChat(String chat)
