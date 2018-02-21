bf24d149490fdc5bd67c90d3e6a826b66a8e5412
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-02-21 12:17:35 -0700

    Added a new class CTECTwitter and wrote the data members and necessary constructor and method header.

---
+++ src/chat/model/CTECTwitter.java
@@ -0,0 +1,25 @@
+package chat.model;
+
+import chat.controller.ChatbotController;
+
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.Status;
+
+public class CTECTwitter
+{
+	private ChatbotController appController;
+	private Twitter chatbotTwitter
+	
+	public CTECTwitter(ChatbotController appController)
+	{
+		this.appController = appContorller;
+		this.chatbotTwitter = TwitterFactory.getSingleton();
+	}
+	
+	public void sendTweet(String textToTweet)
+	{
+		
+	}
+}
