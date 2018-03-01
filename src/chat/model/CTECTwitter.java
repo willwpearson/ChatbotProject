db387e54fcf1bb6a39449112e2d68196473b439d
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-01 11:50:43 -0700

    Added the necessary imports and data members in the twitter class.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,21 +1,38 @@
 package chat.model;
 
 import chat.controller.ChatbotController;
+import chat.controller.IOController;
 
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class CTECTwitter
 {
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
 		this.appController = appController;
+		this.searchedTweets = new ArrayList<Status>();
+		this.tweetedWords = new ArrayList<String>();
 		this.chatbotTwitter = TwitterFactory.getSingleton();
+		this.totalWordCount = 0;
 	}
 	
 	public void sendTweet(String textToTweet)
