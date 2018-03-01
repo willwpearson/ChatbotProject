9892d189aa8f3a3210d7a18411dd552252841d1b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-01 12:31:58 -0700

    Added the method to remove the punctuation to allow the words to be counted properly.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -105,4 +105,19 @@ public class CTECTwitter
 			}
 		}
 	}
+	
+	private String removePunctuation(String currentString)
+	{
+		String punctuation = ".,'?!:;\"(){}^[]<>-";
+		
+		String scrubbedString = "";
+		for(int i = 0; i < currentString.length(); i++)
+		{
+			if(punctuation.indexOf(currentString.charAt(i)) == -1)
+			{
+				scrubbedString += currentString.charAt(i);
+			}
+		}
+		return scrubbedString;
+	}
 }
