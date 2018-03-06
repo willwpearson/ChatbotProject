b0b5185bc0d46a64a02f80a25c1bde1514852302
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-06 12:48:23 -0700

    Added the method to remove the common words from the array.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -59,6 +59,7 @@ public class CTECTwitter
 		turnStatusesToWords();
 		totalWordCount = tweetedWords.size();
 		String [] boring = createIgnoredWordArray();
+		trimTheBoringWords(boring);
 		return mostCommon;
 	}
 	
@@ -151,5 +152,20 @@ public class CTECTwitter
 		
 		wordScanner.close();
 		return boringWords;
+	}
+	
+	private void trimTheBoringWords(String [] boringWords)
+	{
+		for(int index = tweetedWords.size() - 1; index >= 0; index--)
+		{
+			for(int removeIndex = 0; removeIndex < boringWords.length; removeIndex++)
+			{
+				if(tweetedWords.get(index).equals(boringWords[removeIndex]))
+				{
+					tweetedWords.remove(index);
+					removeIndex = boringWords.length;
+				}
+			}
+		}
 	}
 }
