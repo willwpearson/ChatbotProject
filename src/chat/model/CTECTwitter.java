9b2bfd8caec68b15fb359a73173ab5fb9cedaf53
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-03-08 13:34:52 -0700

    Updated new variable of hash map type.

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -16,6 +16,7 @@ import twitter4j.Status;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.text.DecimalFormat;
 
 public class CTECTwitter
@@ -25,6 +26,7 @@ public class CTECTwitter
 	private List<Status> searchedTweets;
 	private List<String> tweetedWords;
 	private long totalWordCount;
+	private HashMap<String, Integer> wordsAndCount; 
 	
 	public CTECTwitter(ChatbotController appController)
 	{
@@ -32,6 +34,7 @@ public class CTECTwitter
 		this.searchedTweets = new ArrayList<Status>();
 		this.tweetedWords = new ArrayList<String>();
 		this.chatbotTwitter = TwitterFactory.getSingleton();
+		this.wordsAndCount = new HashMap<String, Integer>();
 		this.totalWordCount = 0;
 	}
 	
@@ -100,6 +103,7 @@ public class CTECTwitter
 		for(Status currentStatus : searchedTweets)
 		{
 			String tweetText = currentStatus.getText();
+			tweetText = tweetText.replace("\n", " ");
 			String [] tweetWords = tweetText.split(" ");
 			for(int index = 0; index < tweetWords.length; index++)
 			{
